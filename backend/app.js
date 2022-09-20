63186082c71bc6bc35e25348591675fe6b0149f2
sguha-work <sguha1988.life@gmail.com>
2022-09-20 13:37:15 +0530

    https setup

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,8 @@ import express from 'express';
 import bodyParser from 'body-parser';// body parser is required to parse the json data which is passed to the API
 import dotenv from 'dotenv';
 import swaggerUi from 'swagger-ui-express';
+import fs from 'fs';
+import https from 'https';
 
 import swaggerDocument from './swagger.json' assert { type: 'json' };
 import * as teachersRouter from './modules/teachers/routes/teacher_routes.js';
@@ -20,6 +22,11 @@ app.use(function (req, res, next) {
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/teachers', teachersRouter.default);
 
-app.listen(process.env.PORT, () => {
+// getting pem and cert file for https setup
+const key = fs.readFileSync('./key.pem');
+const cert = fs.readFileSync('./cert.pem');
+const server = https.createServer({key: key, cert: cert }, app);
+
+server.listen(process.env.PORT, () => {
     console.log(`App is running on port ${process.env.PORT}`);
 });
